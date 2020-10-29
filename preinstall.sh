d31851a83f62f03d45d524ffcf559c78a230ca8f
justin.lemay <justin.lemay@live.com>
2020-10-29 09:53:28 -0700

    Changed the partitioning

--- preinstall.sh
+++ preinstall.sh
@@ -76,12 +76,12 @@ echo "-- Bootloader Systemd Installation  --"
 echo "--------------------------------------"
 bootctl install
 cat <<EOF > /boot/loader/entries/arch.conf
-title Arch Linux  
-linux /vmlinuz-linux  
-initrd  /initramfs-linux.img  
+title Arch Linux
+linux /vmlinuz-linux
+initrd  /initramfs-linux.img
 options root=${DISK}1 rw
 EOF
-
+arch-chroot /mnt
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
